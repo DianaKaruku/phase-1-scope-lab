638b1ea32b3dd433b949dc6c16e24eed32589211
Diana Karuku <diana.karuku@student.moringaschool.com>
2023-09-25 22:56:14 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName= "bob";
+function customerName(){
+    return customerName;
+}
+
+function upperCaseCustomerName() {
+      customerName = customerName.toUpperCase();
+  }
+
+  function setBestCustomer() {
+    bestCustomer="not bob";
+  }
+
+  function overwriteBestCustomer(){
+    bestCustomer="maybe bob";
+  }
+const leastFavoriteCustomer=""
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer="unsuccessful"
+}
